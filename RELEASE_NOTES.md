8c9ce5d86e830866a2dde9c534b4a9c00acfde64
krauthaufen <krauthaufen@awx.at>
2019-09-19 13:28:11 +0200

    removed Aardvark.Fake dependency

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+### 0.0.1-prerelease01
+initial version
--- build.fsx
+++ build.fsx
@@ -1,26 +1,59 @@
 #r "paket: groupref Build //"
 #load ".fake/build.fsx/intellisense.fsx"
-#load @"paket-files/build/aardvark-platform/aardvark.fake/DefaultSetup.fsx"
 
 open System
 open System.IO
-open System.Diagnostics
-open Aardvark.Fake
 open Fake.Core
 open Fake.Core.TargetOperators
 open Fake.DotNet
 
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-DefaultSetup.install ["FsIncremental.sln"]
+let notes = ReleaseNotes.load "RELEASE_NOTES.md"
 
+Target.create "Clean" (fun _ ->
+    DotNet.exec id "clean" "" |> ignore
+)
+
+Target.create "Compile" (fun _ ->
+    let options (o : DotNet.BuildOptions) =
+        let v = sprintf "%d.%d.%d.%s" notes.SemVer.Major notes.SemVer.Minor notes.SemVer.Patch (string notes.SemVer.Build)
+
+        { o with 
+            Configuration = DotNet.BuildConfiguration.Release
+            
+            MSBuildParams = 
+                { o.MSBuildParams with 
+                    Properties = 
+                        [
+                            "AssemblyVersion", v
+                        ] 
+                }
+        }
+    DotNet.build options "FsIncremental.sln"
+)
+
+
+
+Target.create "Pack" (fun _ ->
+    
+    Paket.pack (fun o ->
+        { o with
+            
+            WorkingDir = Environment.CurrentDirectory
+            OutputPath = "bin"
+            PinProjectReferences = true
+            ProjectUrl = "https://github.com/krauthaufen/FsIncremental"
+            Version = notes.NugetVersion
+            ReleaseNotes = String.concat "\n" notes.Notes
+        }
+    )
+)
 
 Target.create "Test" (fun _ ->
     let options (o : DotNet.TestOptions) =
-        
         { (o.WithRedirectOutput false) with
-            //Common = { o.Common with Verbosity = Some DotNet.Verbosity.Quiet }
-            //ListTests = true
+            Configuration = DotNet.BuildConfiguration.Release
             Logger = Some "console;verbosity=normal"
         }
     DotNet.test options "FsIncremental.sln"
@@ -34,6 +67,17 @@ Target.create "Start" (fun _ ->
 )
 
 Target.create "Run" (fun _ -> Target.run 1 "Start" [])
-"Compile" ==> "Run"
 
-entry()
+Target.create "Default" ignore
+
+
+"Compile" ==> "Test" ==> "Pack"
+"Compile" ==> "Run"
+"Compile" ==> 
+    "Test" ==>
+    "Default"
+
+
+Target.runOrDefault "Default"
+
+
