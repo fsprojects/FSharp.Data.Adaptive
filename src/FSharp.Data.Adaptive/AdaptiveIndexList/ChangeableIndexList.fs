b0144d686907e0390694bb0085496d572d751c27
krauthaufen <krauthaufen@awx.at>
2019-09-28 12:56:41 +0200

    ChangeableIndexList documentation

--- src/FSharp.Data.Adaptive/AdaptiveIndexList/ChangeableIndexList.fs
+++ src/FSharp.Data.Adaptive/AdaptiveIndexList/ChangeableIndexList.fs
@@ -2,7 +2,7 @@
 
 open FSharp.Data.Traceable
 
-/// Changeable adaptive map that allows mutation by user-code and implements amap.
+/// Changeable adaptive list that allows mutation by user-code and implements alist.
 [<Sealed>]
 type ChangeableIndexList<'T>(initial: IndexList<'T>) =
     let history = 
@@ -13,10 +13,13 @@ type ChangeableIndexList<'T>(initial: IndexList<'T>) =
     override x.ToString() =
         history.State |> Seq.map (sprintf "%A") |> String.concat "; " |> sprintf "clist [%s]"
 
-    member x.Count = history.State.Count
-
+    /// is the list currently empty?
     member x.IsEmpty = history.State.IsEmpty
 
+    /// the number of elements currently in the list.
+    member x.Count = history.State.Count
+
+    /// Gets or sets the value for the list.
     member x.Value
         with get() = 
             history.State
@@ -26,22 +29,79 @@ type ChangeableIndexList<'T>(initial: IndexList<'T>) =
             if not (IndexListDelta.isEmpty delta) then
                 history.Perform delta |> ignore
 
+    /// Appends an element to the list and returns its Index.
     member x.Append (element: 'T) =
         let index = Index.after history.State.MaxIndex
         history.Perform(IndexListDelta.single index (Set element)) |> ignore
         index
        
+    /// Appends an element to the list and returns its Index.
+    member x.Add (element: 'T) = x.Append(element)
+
+    /// Prepends an element to the list and returns its Index.
     member x.Prepend (element: 'T) =
         let index = Index.before history.State.MinIndex
         history.Perform(IndexListDelta.single index (Set element)) |> ignore
         index
        
+    /// Inserts an element at the given position in the list and returns its Index.
+    /// Note that the position can be equal to the count of the list.
+    member x.InsertAt(index : int, element : 'T) =
+        if index < 0 || index > history.State.Count then raise <| System.IndexOutOfRangeException()
+        let l, s, r = MapExt.neighboursAt index history.State.Content
+        let r = 
+            match s with
+                | Some s -> Some s
+                | None -> r
+
+        let index = 
+            match l, r with
+            | Some (before,_), Some (after,_) -> Index.between before after
+            | None,            Some (after,_) -> Index.before after
+            | Some (before,_), None           -> Index.after before
+            | None,            None           -> Index.after Index.zero
+
+        history.Perform (IndexListDelta.single index (Set element)) |> ignore
+        index
+
+    /// Inserts an element directly after the given index and returns the new index for the element.
+    member x.InsertAfter(index : Index, element : 'T) =
+        let _l, s, r = MapExt.neighbours index history.State.Content
+        match s with
+        | None ->
+            history.Perform (IndexListDelta.single index (Set element)) |> ignore
+            index
+        | Some (s, _) ->
+            let index =
+                match r with
+                | Some (r,_) -> Index.between s r
+                | None -> Index.after index
+            history.Perform (IndexListDelta.single index (Set element)) |> ignore
+            index
+            
+    /// Inserts an element directly before the given index and returns the new index for the element.
+    member x.InsertBefore(index : Index, element : 'T) =
+        let l, s, _r = MapExt.neighbours index history.State.Content
+        match s with
+        | None ->
+            history.Perform (IndexListDelta.single index (Set element)) |> ignore
+            index
+        | Some (s, _) ->
+            let index =
+                match l with
+                | Some (l,_) -> Index.between l s
+                | None -> Index.after index
+            history.Perform (IndexListDelta.single index (Set element)) |> ignore
+            index
+
+    /// Gets or sets an element in the list at the given index.
     member x.Item
         with get (index: Index) = 
             history.State.[index]
         and set (index: Index) (value: 'T) = 
             history.Perform (IndexListDelta.single index (Set value)) |> ignore
         
+    /// Gets or sets the element associated to index.
     member x.Item
         with get (index: int) = 
             if index < 0 || index >= history.State.Count then raise <| System.IndexOutOfRangeException()
@@ -61,24 +121,52 @@ type ChangeableIndexList<'T>(initial: IndexList<'T>) =
 
             history.Perform (IndexListDelta.single index (Set value)) |> ignore
 
+    /// Clears the list.
+    member x.Clear() =  
+        if not history.State.IsEmpty then
+            let ops = IndexList.computeDelta history.State IndexList.empty
+            history.Perform ops |> ignore        
+
+    /// Removes the given index from the list and returns true if the element was deleted.
     member x.Remove (index: Index) =
         history.Perform(IndexListDelta.single index Remove)
         
+    /// Removes the element at the given position and returns its Index.
     member x.RemoveAt (index: int) =
         if index < 0 || index >= history.State.Count then raise <| System.IndexOutOfRangeException()
         let (index, _) = history.State.Content |> MapExt.item index
         history.Perform(IndexListDelta.single index Remove) |> ignore
+        index
 
+    /// Tries to get the Index associated to the given position.
+    member x.TryGetIndex(index : int) =
+        history.State.TryGetIndex index
+
+    /// Gets the (optional) element associated to the given Index.
+    member x.TryGet(index : Index) =
+        IndexList.tryGet index history.State
+
+    /// Tries to get the element at the given position.
+    member x.TryAt(index : int) =
+        IndexList.tryAt index history.State
+
+
+    /// The smallest index contained in the list (or Index.zero if empty)
     member x.MinIndex = history.State.MinIndex
+
+    /// The largest index contained in the list (or Index.zero if empty)
     member x.MaxIndex = history.State.MaxIndex
 
-    new() = ChangeableIndexList IndexList.empty
+    /// Creates a new list initially holding the given elements.
     new(elements : seq<'T>) = ChangeableIndexList (IndexList.ofSeq elements)
+
+    /// Creates a new empty list.
+    new() = ChangeableIndexList IndexList.empty
 
     interface alist<'T> with
         member x.IsConstant = false
         member x.Content = history :> _
         member x.GetReader() = history.NewReader()
 
-
+/// Changeable adaptive list that allows mutation by user-code and implements alist.
 type clist<'T> = ChangeableIndexList<'T>
---
+++ src/FSharp.Data.Adaptive/AdaptiveIndexList/ChangeableIndexList.fsi
@@ -0,0 +1,78 @@
+﻿namespace FSharp.Data.Adaptive
+
+open FSharp.Data.Traceable
+
+/// Changeable adaptive list that allows mutation by user-code and implements alist.
+[<Sealed>]
+type ChangeableIndexList<'T> =
+    interface AdaptiveIndexList<'T>
+
+    /// is the list currently empty?
+    member IsEmpty : bool
+
+    /// the number of elements currently in the list.
+    member Count : int
+
+    /// Gets or sets the value for the list.
+    member Value : IndexList<'T> with get, set
+
+    /// Gets or sets an element in the list at the given index.
+    member Item : index: int -> 'T with get, set
+
+    /// Appends an element to the list and returns its Index.
+    member Add : element: 'T -> Index
+    
+    /// Appends an element to the list and returns its Index.
+    member Append : element: 'T -> Index
+
+    /// Prepends an element to the list and returns its Index.
+    member Prepend : element: 'T -> Index
+
+    /// Inserts an element at the given position in the list and returns its Index.
+    /// Note that the position can be equal to the count of the list.
+    member InsertAt : index: int * element: 'T -> Index
+
+    /// Removes the element at the given position and returns its Index.
+    member RemoveAt : index: int -> Index
+
+    /// Clears the list.
+    member Clear : unit -> unit
+
+    /// Tries to get the Index associated to the given position.
+    member TryGetIndex : index: int -> option<Index>
+
+    /// Tries to get the element at the given position.
+    member TryAt : index: int -> option<'T>
+    
+    /// The smallest index contained in the list (or Index.zero if empty)
+    member MinIndex : Index
+
+    /// The largest index contained in the list (or Index.zero if empty)
+    member MaxIndex : Index
+
+    /// Gets or sets the element associated to index.
+    member Item : index: Index -> 'T with get, set
+
+    /// Removes the given index from the list and returns true if the element was deleted.
+    member Remove : index: Index -> bool
+
+    /// Inserts an element directly after the given index and returns the new index for the element.
+    member InsertAfter : index: Index * element: 'T -> Index
+
+    /// Inserts an element directly before the given index and returns the new index for the element.
+    member InsertBefore : index: Index * element: 'T -> Index
+
+    /// Gets the (optional) element associated to the given Index.
+    member TryGet : index: Index -> option<'T>
+
+    /// Creates a new list initially holding the given elements.
+    new : elements: IndexList<'T> -> ChangeableIndexList<'T>
+
+    /// Creates a new list initially holding the given elements.
+    new : elements: seq<'T> -> ChangeableIndexList<'T>
+
+    /// Creates a new empty list.
+    new : unit -> ChangeableIndexList<'T>
+
+/// Changeable adaptive list that allows mutation by user-code and implements alist.
+type clist<'T> = ChangeableIndexList<'T>
