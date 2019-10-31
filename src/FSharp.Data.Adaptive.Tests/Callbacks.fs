e1cc9df64db4647cdc12e02c2719823f160c20a0
Georg Haaser <krauthaufen@awx.at>
2019-10-31 10:40:54 +0100

    reworked Callbacks a little * only one `MultiCallbackObject` per object in the graph now * added two simple tests for callbacks

---
+++ src/FSharp.Data.Adaptive.Tests/Callbacks.fs
@@ -0,0 +1,75 @@
+﻿module Callbacks
+
+open FsUnit
+open FsCheck.NUnit
+open FSharp.Data.Adaptive
+open NUnit.Framework
+open System.Threading
+
+[<Test>]
+let ``[MarkingCallback] fired`` () =
+    let m = cval 10
+    let d = m |> AVal.map (fun v -> v)
+
+    let mutable fired = 0
+    let callback() =
+        Interlocked.Increment(&fired) |> ignore
+        
+    let wasFired() =
+        Interlocked.Exchange(&fired, 0)
+
+
+    use __ = d.AddMarkingCallback(callback)
+    wasFired() |> should equal 0
+
+    AVal.force d |> ignore
+    wasFired() |> should equal 0
+
+    transact (fun () -> m.Value <- 100)
+    wasFired() |> should equal 1
+
+    
+    transact (fun () -> m.Value <- 20)
+    wasFired() |> should equal 0
+
+    AVal.force d |> ignore
+    wasFired() |> should equal 0
+
+    
+    transact (fun () -> m.Value <- 15)
+    wasFired() |> should equal 1
+
+
+[<Test>]
+let ``[OnNextMarking] fired`` () =
+    
+    let m = cval 10
+    let d = m |> AVal.map (fun v -> v)
+
+    let mutable fired = 0
+    let callback() =
+        Interlocked.Increment(&fired) |> ignore
+        
+    let wasFired() =
+        Interlocked.Exchange(&fired, 0)
+
+
+    use __ = d.OnNextMarking(callback)
+    wasFired() |> should equal 0
+    
+    AVal.force d |> ignore
+    wasFired() |> should equal 0
+
+    transact (fun () -> m.Value <- 100)
+    wasFired() |> should equal 1
+
+    
+    transact (fun () -> m.Value <- 20)
+    wasFired() |> should equal 0
+
+    AVal.force d |> ignore
+    wasFired() |> should equal 0
+
+    
+    transact (fun () -> m.Value <- 15)
+    wasFired() |> should equal 0
--- src/FSharp.Data.Adaptive/Core/Callbacks.fs
+++ src/FSharp.Data.Adaptive/Core/Callbacks.fs
@@ -2,28 +2,61 @@
 
 open System
 open System.Threading
+open System.Runtime.CompilerServices
 
-/// Represents a callback in the dependency-tree
-type internal CallbackObject(obj: IAdaptiveObject, callback: CallbackObject -> unit) =
+
+/// Represents an object providing callbacks in the dependency-tree
+type internal MultiCallbackObject(table : ConditionalWeakTable<IAdaptiveObject, MultiCallbackObject>, obj: IAdaptiveObject) = 
+    static let mutable id = 0
     static let emptyOutputs = EmptyOutputSet() :> IWeakOutputSet
     let mutable level = obj.Level + 1
-    let mutable live = 1
+    let mutable cbs : HashMap<int, unit -> bool> = HashMap.empty
     let mutable obj = obj
     let mutable weak = null
 
-    member x.Mark() =
-        if live > 0 then callback x
-        false
-
-    member x.Dispose() =
-        if Interlocked.Exchange(&live, 0) = 1 then
+    let check (x : MultiCallbackObject) =
+        if cbs.Count = 0 && not (isNull (obj :> obj)) then
             obj.Outputs.Remove x |> ignore
-            obj <- Unchecked.defaultof<_>
-            weak <- null
-            level <- 0
+            if lock table (fun () -> table.Remove obj) then
+                level <- 0
+                obj <- Unchecked.defaultof<_>
+                weak <- null
+                false
+            else
+                true
+        else
+            true
 
-    interface IDisposable with
-        member x.Dispose() = x.Dispose()
+    let remove (x : MultiCallbackObject) (id : int) =
+        lock x (fun () ->
+            cbs <- HashMap.remove id cbs
+            check x |> ignore
+        )
+
+    /// adds a callback to the object. the returned boolean indicates whether the callback
+    /// should automatically re-subscribe after being fired.
+    member x.Subscribe(cb : unit -> bool) =
+        lock x (fun () ->
+            let i = 
+                #if !FABLE_COMPILER
+                Interlocked.Increment(&id)
+                #else
+                let a = id in id <- a + 1; a
+                #endif
+
+            if cbs.Count = 0 then
+                obj.Outputs.Add x |> ignore
+                
+            cbs <- HashMap.add i cb cbs
+            { new IDisposable with member __.Dispose() = remove x i }
+        )
+    
+    member x.Mark() =
+        cbs <- cbs |> HashMap.filter (fun _ cb -> try cb() with _ -> true)
+        if check x then
+            obj.Outputs.Add x |> ignore
+
+        false
 
     interface IAdaptiveObject with
         member x.Tag
@@ -49,40 +82,45 @@ type internal CallbackObject(obj: IAdaptiveObject, callback: CallbackObject -> u
         member x.Level
             with get() = level
             and set l = level <- l
-
+ 
 /// IAdaptiveObject extensions for creating/removing callbacks.
 [<AutoOpen>]
 module CallbackExtensions =
-    
+    /// cache for MultiCallbackObjects per IAdaptiveObject
+    let private callbackObjects = ConditionalWeakTable<IAdaptiveObject, MultiCallbackObject>()
+
+    /// utility getting/creating a MultiCallbackObjects for the given IAdaptiveObject  
+    let private getMultiCallback (o : IAdaptiveObject) =    
+        lock callbackObjects (fun () ->
+            match callbackObjects.TryGetValue o with
+            | (true, cbo) -> cbo
+            | _ -> 
+                let cbo = MultiCallbackObject(callbackObjects, o)
+                callbackObjects.Add(o, cbo)
+                cbo
+        )
+
+
     type IAdaptiveObject with
         /// Registers a callback with the given object that will be executed
         /// whenever the object gets marked out-of-date.
         /// Note that it does not trigger when the object is currently out-of-date.
         /// Returns a disposable for removing the callback.
         member x.AddMarkingCallback (callback: unit -> unit) =
-            let cb =
-                new CallbackObject(x, fun self ->
-                    try callback ()
-                    finally x.Outputs.Add self |> ignore
-                )
-
-            x.Outputs.Add cb |> ignore
-            cb :> IDisposable
+            let cbo = getMultiCallback x
+            cbo.Subscribe(fun () -> callback(); true)
             
         /// Registers a callback with the given object that will be executed
         /// ONCE! when the next out-of-date marking visits the object.
         /// Note that it does not trigger when the object is currently out-of-date.
         /// Returns a disposable for removing the callback.
         member x.OnNextMarking (callback: unit -> unit) =
-            let cb =
-                new CallbackObject(x, fun self ->
-                    try
-                        callback ()
-                        self.Dispose()
-                    with :? LevelChangedException ->
-                        x.Outputs.Add self |> ignore
-                        reraise()
-                )
-
-            x.Outputs.Add cb |> ignore
-            cb :> IDisposable
+            let cbo = getMultiCallback x
+            cbo.Subscribe(fun () -> 
+                try 
+                    callback()
+                    false
+                with :? LevelChangedException ->
+                    true
+            )
+          
