46dcf6d050e259010f50803d3be0b57b8f0316a6
krauthaufen <krauthaufen@awx.at>
2022-03-21 00:00:50 +0100

    updated build

--- build.sh
+++ build.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-if [ ! -f .paket/paket ]; then
-    echo installing paket
-    dotnet tool install Paket --tool-path .paket --version [5.249.2]
-fi
-
-if [ ! -f paket.lock ]; then
-    echo 'running paket install (no paket.lock found)'
-    .paket/paket install
-fi
-
-if [ ! -f packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll ]; then
-    echo running paket restore
-    .paket/paket install
-fi
-
 dotnet tool restore
-
-dotnet packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll build $@ 
+dotnet paket restore
+dotnet build
