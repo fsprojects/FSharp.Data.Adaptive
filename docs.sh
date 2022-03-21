10ec1dbae326c95d3fa7d9bd9802c21aa75bf74c
krauthaufen <krauthaufen@awx.at>
2022-03-21 01:03:07 +0100

    sync

--- docs.sh
+++ docs.sh
@@ -5,6 +5,9 @@ git clone --depth 1 -b gh-pages $URL ./doctmp
 rm -dfr doctmp/**
 cp -R output/** doctmp
 cd doctmp
+
+git config --local user.email "fsdocs@aardvarkians.com"
+git config --local user.name "fsdocs"
 git add .
 git commit -m "Update docs"
 git push origin gh-pages
