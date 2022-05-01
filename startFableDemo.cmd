2795c166c5408c6697504020fd4fd0cd9c5617b4
krauthaufen <krauthaufen@awx.at>
2022-05-01 22:55:30 +0200

    fixed Fable build

--- startFableDemo.cmd
+++ startFableDemo.cmd
@@ -1,3 +1,4 @@
 @echo off
 pushd src/Demo/Fable
+npm install
 dotnet fable watch . --outDir .\fable --run webpack serve
--- startFableDemo.sh
+++ startFableDemo.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 cd src/Demo/Fable
+npm install
 dotnet fable watch . --outDir ./fable --run webpack serve
